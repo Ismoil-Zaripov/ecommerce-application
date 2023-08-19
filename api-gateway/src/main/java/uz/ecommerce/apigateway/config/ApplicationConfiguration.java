84aafae45fd7ef0881a9315bc52221741751343a
Ismoil <ismoilzaripov55@gmail.com>
2023-08-20 00:42:44 +0500

    First commit

---
+++ api-gateway/src/main/java/uz/ecommerce/apigateway/config/ApplicationConfiguration.java
@@ -0,0 +1,44 @@
+package uz.ecommerce.apigateway.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class ApplicationConfiguration {
+    @Bean
+    @Lazy(false)
+    public List<GroupedOpenApi> apis(RouteDefinitionLocator locator) {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        for (RouteDefinition definition : definitions) {
+            log.info("id: " + definition.getId() + "  " + definition.getUri().toString());
+        }
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-SERVICE")).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("-SERVICE", "");
+            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+        });
+        return groups;
+    }
+
+    @Bean
+    public OpenAPI gateWayOpenApi() {
+        return new OpenAPI().info(new Info().title("Demo Application Microservices APIs ")
+                .description("Documentation for all the Microservices in Demo Application")
+                .version("v1.0.0")
+                .contact(new Contact()
+                        .name("Demo Application Development Team")
+                        .email("demo_support@imaginarycompany.com")));
+    }
+}
